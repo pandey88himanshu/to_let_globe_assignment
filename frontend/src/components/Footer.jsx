0cad117592a01e15182ebdc6b3e7cc6278a3e6b5
pandey88himanshu <pandey88himanshu@gmail.com>
2024-07-09 17:29:00 +0530

    add post design

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-  return <div>footer</div>;
+  return (
+    <>
+      <div className="w-full h-[15vh] bg-[#070706] text-white flex items-center justify-between">
+        <div className="px-8 flex items-center justify-between w-full">
+          <div>
+            <ul className="flex items-center flex-col">
+              <Link to="/">Home</Link>
+              <Link to="/">Project</Link>
+              <Link to="/">Contact</Link>
+            </ul>
+          </div>
+          <div>
+            <ul className="flex items-center flex-col">
+              <Link to="/">Services</Link>
+              <Link to="/">FAQ's</Link>
+              <Link to="/">Testimonial</Link>
+            </ul>
+          </div>
+          <div>
+            <ul className="flex items-center flex-col">
+              <Link to="/">Email</Link>
+              <Link to="/">LinkdIn</Link>
+            </ul>
+          </div>
+          <div>
+            <ul className="flex items-center flex-col">
+              <Link to="/">Signup To Our Newsletter</Link>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Footer;
--- frontend/src/components/PostForm.jsx
+++ frontend/src/components/PostForm.jsx
@@ -3,31 +3,57 @@ import React from "react";
 const PostForm = () => {
   return (
     <>
-      <div>
+      <div className="w-full h-[70vh] text-white bg-[#070706] flex justify-center">
         <div>
-          <form action="">
-            <label htmlFor="title">Enter Post Title</label>
-            <input
-              type="text"
-              placeholder="Enter Here"
-              name="title"
-              id="title"
-            />
-            <label htmlFor="author">Enter Post Author</label>
-            <input
-              type="text"
-              placeholder="Enter Here"
-              name="author"
-              id="author"
-            />
-            <label htmlFor="description">Enter Post Description</label>
-            <textarea
-              name="description"
-              id="description"
-              placeholder="Enter Here"
-            />
-            <label htmlFor="image">Choose Your Image</label>
-            <input type="file" id="image" />
+          <div className=" text-center">
+            <p>Add Post</p>
+          </div>
+          <form action="" className=" flex flex-col gap-6">
+            <div className="flex items-start flex-col">
+              <label htmlFor="title">Enter Post Title</label>
+              <input
+                className="w-[60vw]"
+                type="text"
+                placeholder="Enter Here"
+                name="title"
+                id="title"
+              />
+            </div>
+            <div className=" flex flex-col gap-6">
+              <label htmlFor="author">Enter Post Author</label>
+              <input
+                className="w-[60vw]"
+                type="text"
+                placeholder="Enter Here"
+                name="author"
+                id="author"
+              />
+            </div>
+            <div className=" flex flex-col gap-6">
+              <label htmlFor="description">Enter Post Description</label>
+              <textarea
+                className="w-[60vw]"
+                name="description"
+                id="description"
+                placeholder="Enter Here"
+              />
+            </div>
+            <div className=" flex flex-col gap-6">
+              <label htmlFor="image">Choose Your Image</label>
+              <input
+                type="file"
+                id="image"
+                className="w-[60vw] bg-white text-black"
+              />
+            </div>
+            <div className="flex gap-4">
+              <button className="px-6 py-2 bg-green-500 text-white ">
+                Create Post
+              </button>
+              <button className="px-6 py-2 bg-red-500 text-white ">
+                Reset Content
+              </button>
+            </div>
           </form>
         </div>
       </div>
